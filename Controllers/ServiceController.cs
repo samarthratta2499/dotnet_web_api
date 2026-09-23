def77309cc637b536dad0305f8e90e520c30bf20
samarth <samarth.ratta@dga.or.th>
2026-09-23 18:10:34 +0700

    ServiceController

---
+++ Controllers/ServiceController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_web_api.Controllers;
+
+[ApiController]
+[Route("[controller]/[action]")]
+public class ServiceController : ControllerBase
+{
+    private readonly IService _service;
+
+    public ServiceController(IService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Get(string id, CancellationToken ct)
+    {
+        var user = await _service.GetAsync(id, ct);
+        return Ok(user);
+    }
+}
