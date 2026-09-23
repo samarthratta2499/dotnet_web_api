fe9685b5f847a1b1385833742131175600b21df6
samarth <samarth.ratta@dga.or.th>
2026-09-23 19:19:45 +0700

    Refactor ServiceController to UsersController with updated route and constructor parameters

--- Controllers/ServiceController.cs
+++ Controllers/ServiceController.cs
@@ -3,23 +3,30 @@ using Microsoft.AspNetCore.Mvc;
 namespace dotnet_web_api.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class ServiceController : ControllerBase
+[Route("api/org/v{version:apiVersion}/users")]
+public class UsersController : ControllerBase
 {
-    private readonly IService _service;
+    private readonly IUserApplicationService _service;
+    private readonly IOrgIdentityContext _identityContext;
+    private readonly IOrgAuthorizationService _authorization;
 
-    public ServiceController(IService service)
+    public UsersController(
+        IUserApplicationService service,
+        IOrgIdentityContext identityContext,
+        IOrgAuthorizationService authorization)
     {
         _service = service;
+        _identityContext = identityContext;
+        _authorization = authorization;
     }
 
     [HttpGet("{id}")]
     public async Task<IActionResult> Get(string id, CancellationToken ct)
     {
-        var user = await _service.GetAsync(id, ct); 
-        var traceId = Activity.Current?.TraceId.ToString() ?? HttpContext.TraceIdentifier; 
+        var user = await _service.GetAsync(id, ct);
+        var traceId = Activity.Current?.TraceId.ToString() ?? HttpContext.TraceIdentifier;
         return Ok(OrgApiResponse<UserDto>.Success(user, traceId));
     }
-
-
 }
+
+
